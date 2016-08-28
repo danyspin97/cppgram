a26c766613d32233c8b309ef8e9a4e2d7cf2f6a8
Stefano Belli <sa.belli@hotmail.it>
2016-08-29 00:35:49 +0200

    Update headers

--- include/cppgram/corebot.h
+++ include/cppgram/corebot.h
@@ -5,14 +5,38 @@
 
 namespace cppgram
 {
+    typedef union {
+        struct msg {
+            long message_id;
+            //...
+        } message;
+
+        struct cbquery {
+            //...
+            union chat_id {
+
+            } chatId;
+        } callbackQuery;
+    } update_t ;
+
+    typedef struct update_t::msg update_msg_t;
+    typedef struct update_t::cbquery update_cbquery_t;
+
     class CoreBot : public Logger
     {
     public:
         CoreBot(const std::string& api_token,
                 const std::ofstream& output);
-        //~CoreBot();
+        void runLoop() const;
+    protected:
+        virtual void processMessage(const update_msg_t& data) const;
+        virtual void processInlineQuery(const update_cbquery_t& data) const;
+        void sendMessage(const std::string& msg) const;
+        //editMessage
     private:
         const std::string botok;
+        unsigned long lastUpdateId;
+        void getUpdates() const;
     };
 }
 
